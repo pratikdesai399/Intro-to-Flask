df556ad59b8c018fc37a3cfbeb8424b036a0613b
Pratik Desai <pratikdesai399@gmail.com>
2024-05-23 18:38:32 +0530

    flask routes

---
+++ README.md
@@ -0,0 +1,34 @@
+# Flask Application
+
+## Setup
+
+### Prerequisites
+
+- Python 3 installed on your system.
+
+### Steps
+
+1. **Clone the Repository**
+
+   ```sh
+   git clone <your-repo-url>
+   cd <your-repo-name>
+
+2. **Create Virtual Environment**
+
+    ```sh
+    python3 -m venv .venv
+
+3. Activate the Virtual Environment
+
+    On macOS and Linux:
+
+    ```sh
+    source .venv/bin/activate
+
+4. Install Flask
+
+    ```sh
+    pip3 install flask
+
+    
--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/post_method', methods=['POST'])
+def hello_post():
+    return 'POST'
+
+@app.route('/greet/<name>')
+def hello(name):
+    return f"Hello {name}" 
+
+
+@app.route('/handle_url_params')
+def handle_params():
+    return str(request.args)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5555, debug=False)
+    app.run(host='127.0.0.1', port=5555, debug=True)
